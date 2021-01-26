0c3d87f053779469cffc516e711cd859a4dbdd7b
Chathura Hewa Algiriyage <cpriyankara@ecs.vuw.ac.nz>
2021-01-27 11:22:09 +1300

    Login with DB connection

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const StudentModel = require('./models/Student');
+const UserModel = require('./models/User');
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://cpriyankara:admin@cluster0.otque.mongodb.net/student-management?retryWrites=true&w=majority',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect('mongodb+srv://cpriyankara:admin@cluster0.otque.mongodb.net/student-management?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.post('/insert',async (req,res)=>{
-    const student = new StudentModel({name:req.body.name, age:req.body.age, degree:req.body.degree});
+app.post('/insert', async (req, res) => {
+    const student = new StudentModel({name: req.body.name, age: req.body.age, degree: req.body.degree});
     await student.save();
     res.send('Inserted data');
 });
 
-app.get('/get-all', async (req,res)=>{
-    await StudentModel.find({},(err, result)=>{
-        if(err) {
+app.get('/get-all', async (req, res) => {
+    await StudentModel.find({}, (err, result) => {
+        if (err) {
             res.send(err);
         } else {
             res.send(result);
@@ -28,31 +29,38 @@ app.get('/get-all', async (req,res)=>{
     });
 });
 
-app.put('/update', async (req, res)=>{
+app.put('/update', async (req, res) => {
     const newName = req.body.newName;
     const id = req.body.id;
     try {
         // await StudentModel.findByIdAndUpdate({_id:id},{name:newName})
-        await StudentModel.findById(id, (error, studentToUpdate)=>{
+        await StudentModel.findById(id, (error, studentToUpdate) => {
             studentToUpdate.name = newName;
             studentToUpdate.save();
         });
-    }catch(err) {
+    } catch (err) {
         console.log(err);
     }
     res.send('Updated');
 });
 
-app.delete('/delete/:id',async(req,res)=>{
+app.delete('/delete/:id', async (req, res) => {
     await StudentModel.findByIdAndDelete(req.params.id).exec();
     res.send("Student Removed.");
 });
 
 app.post('/login', async (req, res) => {
-    //Need to connect this with the database.
-   await req.body.username['username'] === 'ibm' && req.body.password['password'] ==='ibm' ? res.send({login:'success'}) : res.send({login:'fail'})
+    const uname = req.body.username['username'];
+    const pwd = req.body.password['password'];
+    await UserModel.find({username: uname, password: pwd}, (err, result) => {
+        if (result.length === 1) {
+            res.send({login: 'success'});
+        } else {
+            res.send({login: 'fail'})
+        }
+    });
 });
 
-app.listen(3001, ()=>{
+app.listen(3001, () => {
     console.log("Server is listening");
 });
